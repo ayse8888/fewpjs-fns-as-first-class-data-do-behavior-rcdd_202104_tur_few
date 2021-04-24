2d9b36c473c3033fbaca55b986f0dc50365e9447
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-04-24 20:26:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(timeString) {
-const time = parseInt(document.getElementById('time').value)
+// const time = parseInt(document.getElementById('time').value)
   if(time < 12){
     return "Good Morning"
   } else if(time >= 12 && time < 17) {
